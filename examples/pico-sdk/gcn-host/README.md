a4c89bc04f37fd580454f4670885df1784a3dfb6
James Smith <james@loopj.com>
2025-10-29 09:32:49 -0700

    Simplify pico GCN host example

--- examples/pico-sdk/gcn-host/README.md
+++ examples/pico-sdk/gcn-host/README.md
@@ -1,8 +1,17 @@
-# Pico SDK Host Example
+# Pico SDK GameCube Host Example
 
-Act as an Joybus host on RP2XXX devices using the Pico SDK.
+Act as an Joybus host on RP2XXX devices using the Pico SDK. Toggle a GPIO when the A button is pressed on a connected GameCube controller.
+
+## Building
 
 ```bash
-# Build the project
 cmake -Bbuild . && cmake --build build
+```
+
+## Flashing
+
+Enter bootloader mode by holding the BOOTSEL button while plugging in the Pico. Then copy the generated `build/app.uf2` file to the RPI-RP2 drive that appears, or use `picotool`:
+
+```bash
+picotool load -f build/app.uf2
 ```
--- examples/pico-sdk/gcn-host/main.c
+++ examples/pico-sdk/gcn-host/main.c
@@ -6,34 +6,76 @@
 #include <joybus/joybus.h>
 #include <joybus/backend/rp2xxx.h>
 
-#define SI_DATA_GPIO  12
-#define LED_GPIO      13
+#define JOYBUS_GPIO             12
+#define LED_GPIO                13
 
+#define JOYBUS_POLL_INTERVAL_MS 15
+
+// Joybus instance
 struct joybus_rp2xxx rp2xxx_bus;
 struct joybus *bus = JOYBUS(&rp2xxx_bus);
 
-uint8_t response[JOYBUS_BLOCK_SIZE] = {0};
+// Buffer for Joybus responses
+uint8_t joybus_response[JOYBUS_BLOCK_SIZE] = {0};
 
-static uint16_t last_buttons = 0;
-static uint16_t down_buttons = 0;
-static uint16_t up_buttons   = 0;
+// Polling mode
+enum { POLL_MODE_IDENTIFY, POLL_MODE_READ };
+static uint8_t poll_mode = POLL_MODE_IDENTIFY;
 
-void identify_cb(struct joybus *bus, int result, void *user_data)
+void joybus_identify_cb(struct joybus *bus, int result, void *user_data)
 {
+  // Stay in identify mode on any Joybus error
+  if (result < 0)
+    return;
+
+  // Check it's a GameCube controller
+  uint16_t type = joybus_id_get_type(joybus_response);
+  if (!(type & JOYBUS_ID_GCN_DEVICE))
+    return;
+
+  // Check we've received data if it's a wireless controller
+  if ((type & JOYBUS_ID_GCN_WIRELESS) && !(type & JOYBUS_ID_GCN_WIRELESS_RECEIVED))
+    return;
+
+  // Check it's a standard controller
+  if (!(type & JOYBUS_ID_GCN_STANDARD))
+    return;
+
+  // Move to polling for input
+  poll_mode = POLL_MODE_READ;
 }
 
-void read_origin_cb(struct joybus *bus, int result, void *user_data)
+void joybus_read_cb(struct joybus *bus, int result, void *user_data)
 {
+  // Switch back to identify mode on any Joybus error
+  if (result < 0) {
+    poll_mode = POLL_MODE_IDENTIFY;
+    return;
+  }
+
+  // Extract button states
+  uint16_t buttons = ((joybus_response[1] << 8) | joybus_response[0]) & JOYBUS_GCN_BUTTON_MASK;
+
+  // Light the LED while the A button is held
+  gpio_put(LED_GPIO, buttons & JOYBUS_GCN_BUTTON_A);
 }
 
-void read_cb(struct joybus *bus, int result, void *user_data)
+// Poll the Joybus at regular intervals
+static bool poll_task(struct repeating_timer *timer)
 {
-  uint16_t buttons = ((response[1] << 8) | response[0]) & JOYBUS_GCN_BUTTON_MASK;
+  // Poll Joybus for data
+  switch (poll_mode) {
+    case POLL_MODE_IDENTIFY:
+      gpio_put(LED_GPIO, 0);
+      joybus_identify(bus, joybus_response, joybus_identify_cb, NULL);
+      break;
 
-  down_buttons = (buttons ^ last_buttons) & buttons;
-  up_buttons   = (buttons ^ last_buttons) & last_buttons;
+    case POLL_MODE_READ:
+      joybus_gcn_read(bus, JOYBUS_GCN_ANALOG_MODE_3, JOYBUS_GCN_MOTOR_STOP, joybus_response, joybus_read_cb, NULL);
+      break;
+  }
 
-  last_buttons = buttons;
+  return true;
 }
 
 int main()
@@ -46,30 +88,17 @@ int main()
   gpio_set_dir(LED_GPIO, GPIO_OUT);
   gpio_put(LED_GPIO, 0);
 
-  // Initialize and enable Joybus
-  joybus_rp2xxx_init(&rp2xxx_bus, SI_DATA_GPIO, pio0);
+  // Initialize Joybus
+  joybus_rp2xxx_init(&rp2xxx_bus, JOYBUS_GPIO, pio0);
   joybus_enable(bus);
 
-  // Ask any connected targets for their ID
-  joybus_identify(bus, response, identify_cb, NULL);
-  sleep_ms(100);
+  // Poll for Joybus data and send HID reports at regular intervals
+  struct repeating_timer poll_timer;
+  add_repeating_timer_ms(JOYBUS_POLL_INTERVAL_MS, poll_task, NULL, &poll_timer);
 
-  // Request controller origin
-  joybus_gcn_read_origin(bus, response, read_origin_cb, NULL);
-  sleep_ms(100);
-
-  // TODO: We should check for a response, and check if the response is valid
-
-  while (true) {
-    // Read the controller state
-    joybus_gcn_read(bus, JOYBUS_GCN_ANALOG_MODE_3, JOYBUS_GCN_MOTOR_STOP, response, read_cb, NULL);
-
-    // Light the LED while the A button is held
-    gpio_put(LED_GPIO, last_buttons & JOYBUS_GCN_BUTTON_A);
-
-    // Wait a bit before reading again
-    sleep_ms(16);
-  }
+  // No work to do in main loop
+  while (true)
+    ;
 
   return 0;
 }
